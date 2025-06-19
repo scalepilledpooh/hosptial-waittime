71208715aee7e705e3653b3ac97972e119f7010d
Andrew B <apbowlus@gmail.com>
2025-06-18 19:24:03 -0500

    Updated config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.SUPABASE_URL = 'https://example.com';
-window.SUPABASE_ANON_KEY = 'anonkey';
+window.SUPABASE_URL = 'undefined';
+window.SUPABASE_ANON_KEY = 'undefined';
 window.MAPBOX_TOKEN = 'pk.eyJ1Ijoicnlhbi1zaGFyayIsImEiOiJjbWJqeTlrbTIwa3M4MmlzZDh2angyb3o0In0.SmMHpdCg773r-ChA3xZYXA';
